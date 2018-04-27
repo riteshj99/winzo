c5bbf3dba56b587e14dcde88efdeb423b31160b6
gomedii <gomedii@gomediis-Mac-mini.local>
2018-04-27 15:18:31 +0530

    file change

---
+++ TestRichNotification/Winzo-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <JuspaySafeBrowser/JuspaySafeBrowser.h>
+#import <JuspaySafeBrowser/JuspaySafeBrowserViewController.h>
+#import <JuspaySafeBrowser/JuspayCodes.h>
+#import <JuspaySafeBrowser/BrowserParams.h>
+#import <JuspaySafeBrowser/UIViewController+BackButtonHandler.h>
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
